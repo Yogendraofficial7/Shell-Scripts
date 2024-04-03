beed43613ca98a893620bd76fd0e81a3ac947b55
Yogendraofficial7 <myogendrakumar235@gmail.com>
2024-04-03 09:23:24 +0000

    Created CharactersCount.sh File

---
+++ CharactersCount.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+
+# Shell Scripting for a Counting the Characters inside a Sting
+# --------------------------------------------------------
+
+
+	string=$1
+	
+	read -p "Enter a String:" string
+	
+	for char in {a..z}
+	do
+  	    count=`awk -F"${char}" '{print NF-1}' <<< "${string}"`
+  	
+	    if [ $count  != 0 ]; 
+	    then
+    		echo -n $char:
+    		echo $count
+  	    fi
+	done
